027ff0120f77f858180baba5b70c4f59f422ff43
ryuuwatoki <ryuuwatoki@gmail.com>
2025-05-06 21:06:30 +0900

    soundManager.cs編集

--- SoundManager.cs
+++ SoundManager.cs
@@ -2,6 +2,60 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource startBgmAudio;
+    public AudioSource startBtnAudio;
+    public AudioSource gameBgmAudio;
+    public AudioSource getAudio;
+    public AudioSource boomAudio;
+
+    public void PlayStartBgm(){
+        startBgmAudio.Play();
+    }
+
+    public void StopStartBgm(){
+        startBgmAudio.Stop();
+    }
+
+    public void PlayStartBtnAndSwitchBgm()
+    {
+        StartCoroutine(PlayStartBtnThenGameBgm());
+    }
+
+    private System.Collections.IEnumerator PlayStartBtnThenGameBgm()
+    {
+        startBtnAudio.Play();
+        yield return new WaitForSeconds(3f);
+        gameBgmAudio.Play();
+    }
+
+    public void StopGameBgm()
+    {
+        gameBgmAudio.Stop();
+    }
+
+    public void PlayGameBgm()
+    {
+        if (!gameBgmAudio.isPlaying)
+            gameBgmAudio.Play();
+    }
+
+    public void PlayGet()
+    {
+        getAudio.Play();
+    }
+
+    public void PlayBoom()
+    {
+        boomAudio.Play();
+    }
+
+
+
+
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,3 +68,4 @@ public class SoundManager : MonoBehaviour
         
     }
 }
+
