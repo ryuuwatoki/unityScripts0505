7df0aab2682c401833bc9535f9154ef7c49952a3
ryuuwatoki <ryuuwatoki@gmail.com>
2025-05-06 21:50:42 +0900

    ブラックホールの自転機能を追加

--- BlackHold.cs
+++ BlackHold.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class BlackHold : MonoBehaviour
 {
+    // 旋轉速度（每秒幾圈），可在Inspector調整
+    [SerializeField]
+    private float rotationSpeed = 1f; // 預設每秒1圈
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,7 @@ public class BlackHold : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 每秒360度 * 旋轉速度
+        transform.Rotate(0, 0, 360f * rotationSpeed * Time.deltaTime);
     }
 }
