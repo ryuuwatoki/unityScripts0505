5504200d7294884175b4ed5909083c47bee97438
ryuuwatoki <ryuuwatoki@gmail.com>
2025-05-06 13:16:37 +0900

    隕石のランダムコード変更

--- MeteorPickup.cs
+++ MeteorPickup.cs
@@ -8,7 +8,7 @@ public class MeteorPickup : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RandomMeteoritePosition();
     }
 
     // Update is called once per frame
@@ -19,13 +19,23 @@ public class MeteorPickup : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        RandomMeteoritePosition();
+    }
+
+
+
+    // 隕石のランダムな出現位置を指定するための変数
+    void RandomMeteoritePosition()
+    {
         // テスト用のリスポーン範囲
         // var b = MeteoriteArea.bounds;
         // Debug.Log($"{b.min.x}, {b.max.x}, {b.min.y}, {b.max.y}");
 
+        // 隕石のランダムな出現位置を指定する
         transform.position = new Vector2(
             Random.Range(MeteoriteArea.bounds.min.x, MeteoriteArea.bounds.max.x),
             Random.Range(MeteoriteArea.bounds.min.y, MeteoriteArea.bounds.max.y)
         );
     }
+
 }
